68800ae2787890db50db04e8e03c4cfeb2c989da
Fourmisain <8464472+Fourmisain@users.noreply.github.com>
2022-06-14 18:23:42 +0200

    fix build warning

--- src/main/java/io/github/haykam821/sneakyscreens/mixin/GameOptionsMixin.java
+++ src/main/java/io/github/haykam821/sneakyscreens/mixin/GameOptionsMixin.java
@@ -1,23 +1,22 @@
 package io.github.haykam821.sneakyscreens.mixin;
 
+import io.github.haykam821.sneakyscreens.Main;
 import net.minecraft.client.option.GameOptions;
-import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.MinecraftClient;
 
+import org.spongepowered.asm.mixin.Dynamic;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import io.github.haykam821.sneakyscreens.Main;
-
 @Mixin(GameOptions.class)
 public class GameOptionsMixin {
-	@Shadow KeyBinding keySneak;
-	@Shadow boolean sneakToggled;
+	@Shadow public boolean sneakToggled;
 
-	@Inject(at = @At("RETURN"), method = "method_23487", cancellable = true)
+	@Dynamic("KeyBinding keySneak lambda")
+	@Inject(at = @At("RETURN"), method = "method_23487", remap = false, cancellable = true)
     private void load(CallbackInfoReturnable<Boolean> info) {
 		info.setReturnValue(Main.toggleGetter(this.sneakToggled, MinecraftClient.getInstance()));
 	}
